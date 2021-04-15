3ac6fe977030cb109e509d460390906034f67275
John Smoker <testgits@yandex.ru>
2021-04-15 21:56:25 +0500

    Added login tests

--- src/test/java/api/UserTests.java
+++ src/test/java/api/UserTests.java
@@ -3,33 +3,45 @@ package api;
 import api.model.UserModel;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static api.endpoints.Endpoints.USER;
 import static io.restassured.RestAssured.given;
 import static api.spec.Spec.spec;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.*;
 
 public class UserTests {
-    @Test
-    void userTest() {
-        UserModel user = new UserModel();
 
-        user.setUsername("test_username1234");
-        user.setPassword("123321");
-        user.setFirstName("Testinger");
-        user.setLastName("Testingoff");
-        user.setEmail("mamilo@mail.oo");
-        user.setPhone("78789393993");
-
-        String id = given(spec)
+    private String registerUser(UserModel user) {
+        return given(spec)
                 .body(user)
                 .post(USER.getPath())
                 .then()
                 .statusCode(200)
                 .body("message", notNullValue())
                 .extract().path("message").toString();
+    }
 
-        user.setId(id);
+    private void deleteUser(UserModel user) {
+        given(spec)
+                .delete(USER.addPath("/{username}"), user.getUsername())
+                .then()
+                .log().body();
+    }
+
+    @Test
+    void userTest() {
+        UserModel user = new UserModel();
+
+        user.setUsername("test_name1234");
+        user.setPassword("123321");
+        user.setFirstName("Testinger");
+        user.setLastName("Testingoff");
+        user.setEmail("mamilo@mail.oo");
+        user.setPhone("78789393993");
+
+        user.setId(registerUser(user));
 
         given(spec)
                 .get(USER.addPath("/{username}"), user.getUsername())
@@ -59,10 +71,43 @@ public class UserTests {
                 .body("id", notNullValue())
                 .body("phone", equalTo(user.getPhone()));
 
+        deleteUser(user);
+
+    }
+
+    @Test
+    void loginTest() {
+        UserModel user = new UserModel();
+
+        user.setUsername("test_name1234");
+        user.setPassword("123321");
+        user.setFirstName("Testinger");
+        user.setLastName("Testingoff");
+        user.setEmail("mamilo@mail.oo");
+        user.setPhone("78789393993");
+
+        user.setId(registerUser(user));
+
+        Map<String, String> loginUser = new HashMap<String, String>() {{
+            put("username", user.getUsername());
+            put("password", user.getPassword());
+        }};
+
         given(spec)
-                .delete(USER.addPath("/{username}"), user.getUsername())
+                .queryParams(loginUser)
+                .get(USER.addPath("/login"))
                 .then()
-                .log().body();
+                .statusCode(200)
+                .body("message", containsString("logged in"));
+
+        given(spec)
+                .get(USER.addPath("/logout"))
+                .then()
+                .log().body()
+                .statusCode(200);
+
+        deleteUser(user);
+
 
     }
 }
